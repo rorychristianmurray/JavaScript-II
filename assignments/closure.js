4dacec1ab88dd7a8d9f72f580f98917414e3dd54
Rory Murray <rory.christian.murray@gmail.com>
2019-05-07 18:51:24 -0400

    added more work on array-methods.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,17 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function outerClosure() {
+  let outerVal = 10;
+  function innerClosure() {
+    let innerVal = 42;
+    console.log(outerVal + innerVal);
+  }
+}
+
+outerClosure();
+
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
