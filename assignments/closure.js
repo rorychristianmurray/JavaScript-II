1c32461e92e7874762badaf6b39032c8410c90bb
Rory Murray <rory.christian.murray@gmail.com>
2019-05-07 22:54:07 -0400

    added clojure challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,11 +17,23 @@ function outerClosure() {
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+return function() {
+  return ++count;
+}
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+  // Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
